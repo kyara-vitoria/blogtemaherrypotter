037a44074297e9ff2709963aa4715ae5e37b8073
kyara-vitoria <kyara.vitoria.rodrigues@escola.pr.gov.br>
2026-08-25 11:20:33 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+ // BOTÃO CURTIR
+
+        function curtir(botao) {
+
+            botao.innerHTML = "❤️ Curtido!";
+
+            botao.style.backgroundColor = "#7f6024";
+            botao.style.color = "#0b0f19";
+
+        }
+
+
+        // BOTÃO GOSTEI
+
+        function gostei(botao) {
+
+            botao.innerHTML = "👍 Você Gostou!";
+
+            botao.style.backgroundColor = "#7f6024";
+            botao.style.color = "#0b0f19";
+
+        }
+
+
+        // BOTÃO NÃO GOSTEI
+
+        function naoGostei(botao) {
+
+            botao.innerHTML = "👎 Não Gostei";
+
+            botao.style.backgroundColor = "#7f6024";
+            botao.style.color = "#0b0f19";
+
+        }
